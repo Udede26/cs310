b334e8bef093a1fa5242dce59cdd6464872a5f9b
İhsan Ufuk Dede <udede@sabanciuniv.edu>
2021-04-01 01:23:22 +0300

    initial commit

---
+++ lib/login_body.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/login_background.dart';
+import 'package:flutter_app/signup_screen.dart';
+import 'package:flutter_app/already_have_an_account_acheck.dart';
+import 'package:flutter_app/rounded_button.dart';
+import 'package:flutter_app/rounded_input_field.dart';
+import 'package:flutter_app/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_app/welcome.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 150,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child:  RoundedButton(
+                  text: "BACK",
+                  press: () { Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return WelcomeScreen();
+                      },
+                    ),
+                  );},
+                ),
+              ),
+            ),
+
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Image.asset(
+              "assets/icons/book.png",
+              height: size.height * 0.35,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup_body.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/login_screen.dart';
+import 'package:flutter_app/signup_background.dart';
+import 'package:flutter_app/or_divider.dart';
+import 'package:flutter_app/social_icon.dart';
+import 'package:flutter_app/already_have_an_account_acheck.dart';
+import 'package:flutter_app/rounded_button.dart';
+import 'package:flutter_app/rounded_input_field.dart';
+import 'package:flutter_app/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_app/welcome.dart';
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 150,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child:  RoundedButton(
+                  text: "BACK",
+                  press: () { Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return WelcomeScreen();
+                      },
+                    ),
+                  );},
+                ),
+              ),
+            ),
+            Text(
+              "SIGNUP",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Image.asset(
+              "assets/icons/book.png",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
